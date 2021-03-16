f11bd456faac3bbd73da38a6c0342b5ed48e70de
Michiel Auerbach <m.auerbach@rocmondriaan.nl>
2021-03-16 17:18:01 +0100

    update2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 console.log("main.js file loaded");
 
 const htmlBody = document.body;
+const theButtons = document.querySelectorAll('.button_light');
 
 const firstButton = document.querySelector('.switch_theme');
+console.log(theButtons);
+
 if (firstButton) {
     console.log('Hallo goedemiddag meneer de Jong en meneer van der Linden');
+    firstButton.addEventListener('click', changeTheme);
 }
+
+function changeTheme() {
+    htmlBody.classList.toggle('the_body_dark');
+    for (let i = 0; i < theButtons.length; i++) {
+        const changeButton = theButtons[i];
+        changeButton.classList.toggle('button_dark');
+        changeButton.classList.toggle('button_light');
+    }
+
+    console.log('Theme switched');
+}
+
